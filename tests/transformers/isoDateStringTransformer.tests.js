6c6666e7f4ca7cac00a87048e1d7b909b8c5c2f3
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-08-27 08:30:59 +0100

    Fixing iso date time string transformer name

--- tests/transformers/isoDateStringTransformer.tests.js
+++ tests/transformers/isoDateStringTransformer.tests.js
@@ -1,12 +1,12 @@
 'use strict'
 
 var assert = require('assert')
-var isoDateString = require('../../lib/transformers').isoDateString
+var isoDateTimeString = require('../../lib/transformers').isoDateTimeString
 
 describe('iso date string transformer', function() {
 
     it('should convert date to an ISO string', function(done) {
-        isoDateString({}, new Date(), function(err, result) {
+        isoDateTimeString({}, new Date(), function(err, result) {
             assert.ifError(err)
             assert.ok(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(result))
             done()
@@ -14,7 +14,7 @@ describe('iso date string transformer', function() {
     })
 
     it('should convert non date string to an ISO string', function(done) {
-        isoDateString({}, 'Thu Aug 27 2015 08:17:03 GMT+0100 (BST)', function(err, result) {
+        isoDateTimeString({}, 'Thu Aug 27 2015 08:17:03 GMT+0100 (BST)', function(err, result) {
             assert.ifError(err)
             assert.ok(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(result))
             done()
@@ -22,7 +22,7 @@ describe('iso date string transformer', function() {
     })
 
     it('should tolerate falsey values', function(done) {
-        isoDateString({}, null, function(err, result) {
+        isoDateTimeString({}, null, function(err, result) {
             assert.ifError(err)
             assert.equal(result, null)
             done()
@@ -30,7 +30,7 @@ describe('iso date string transformer', function() {
     })
 
     it('should yield errors for unconvertables', function(done) {
-        isoDateString({}, 'not-a-date', function(err, result) {
+        isoDateTimeString({}, 'not-a-date', function(err, result) {
             assert.ok(err)
             done()
         })
